a0ede0a4581285c03a7a9816f325e57546212d3a
Vitaliy <wololo166@gmail.com>
2021-03-22 17:56:35 +0300

    Now all funtcions work properly

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import design
 import os
 import cv2
 import numpy as np
+import h5py
 
 class MainWindow(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
@@ -14,12 +15,16 @@ class MainWindow(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.input_folder_button_2.clicked.connect(self.get_input_folder)
         self.input_folder_button_3.clicked.connect(self.get_input_folder)
         self.input_folder_button_4.clicked.connect(self.get_input_folder)
+        
         self.output_folder_button.clicked.connect(self.get_output_folder)
         self.output_folder_button_2.clicked.connect(self.get_output_folder)
         self.output_folder_button_3.clicked.connect(self.get_output_folder)
         self.output_folder_button_4.clicked.connect(self.get_output_folder)
         
         self.start_button.clicked.connect(self.crop_images)
+        self.start_button_2.clicked.connect(self.crop_masks)
+        self.start_button_3.clicked.connect(self.compress_images)
+        self.start_button_4.clicked.connect(self.compress_masks)
         
     
     def get_input_folder(self):
@@ -68,8 +73,9 @@ class MainWindow(QtWidgets.QMainWindow, design.Ui_MainWindow):
         mask_width = int(self.required_width_edit_2.text())
         mask_height = int(self.required_height_edit_2.text())
         
-        files = next(os.walk(root_path))[2]
+        files = next(os.walk(root_path))[2]         
         print('Total number of files =',len(files))
+        number_of_file = 0
         
         for mask_file in os.listdir(root_path):            
             mask_path = root_path + '/' + mask_file
@@ -77,6 +83,7 @@ class MainWindow(QtWidgets.QMainWindow, design.Ui_MainWindow):
             mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
             mask = mask//38
             
+            number_of_file += 1
             counter = 0
             for r in range(0, mask.shape[0], mask_height):
                 for c in range(0, mask.shape[1], mask_width):
@@ -88,6 +95,65 @@ class MainWindow(QtWidgets.QMainWindow, design.Ui_MainWindow):
                     cv2.imwrite(new_mask_path, blank_mask)  
             value = number_of_file/len(files)*100
             self.progressBar_2.setValue(value)
+            
+    def compress_images(self):
+        root_path = self.input_path_edit_3.text()
+        all_images = []
+        #counter = 0
+        number_of_file = 0
+        files = next(os.walk(root_path))[2]
+        print('Total number of files =',len(files))        
+        
+        for image_file in os.listdir(root_path):
+            #counter += 1
+            
+            image_path = root_path + '/' + image_file
+            image = cv2.imread(image_path)
+            all_images.append(image)
+            
+            number_of_file += 1            
+            value = number_of_file/len(files)*100
+            self.progressBar_3.setValue(value)
+            
+        all_images = np.asarray(all_images)
+        print("Shape of Train Images = ", all_images.shape)
+        self.lineEdit.setText("Total number of files = " + str(len(files)) + '. ' + "Shape of Train Images = " + str(all_images.shape))
+        
+        with h5py.File(self.output_path_edit_3.text() + '/images.h5py', 'w') as hf:
+            hf.create_dataset("all_images", data = all_images)
+            
+        print("Data has been successfully exported.")
+        
+    def compress_masks(self):
+        root_path = self.input_path_edit_4.text()
+        all_masks = []
+        #counter = 0
+        number_of_file = 0
+        files = next(os.walk(root_path))[2]
+        print('Total number of files =',len(files))
+        
+        for mask_file in os.listdir(root_path):
+            #counter += 1
+            
+            mask_path = root_path + '/' + mask_file
+            mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+            all_masks.append(mask)
+            
+            number_of_file += 1            
+            value = number_of_file/len(files)*100
+            self.progressBar_4.setValue(value)
+            
+        all_masks = np.asarray(all_masks)
+        print("Shape of Train masks = ", all_masks.shape)
+        
+        self.lineEdit.setText("Total number of files = " + str(len(files)) + '. ' + "Shape of Train Masks = " + str(all_masks.shape))
+        
+        with h5py.File(self.output_path_edit_3.text() + '/masks.h5py', 'w') as hf:
+            hf.create_dataset("all_masks", data = all_masks)
+            
+        print("Data has been successfully exported.")
+        
+        
 def main():
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
