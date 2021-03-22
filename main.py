b245a553d1b62d88292eed2c9a22db9402f0cbab
Vitaliy <wololo166@gmail.com>
2021-03-22 16:59:40 +0300

    Added functions (tabs): crop masks, compress images and masks. Added working progressbar with %.

--- main.py
+++ main.py
@@ -11,18 +11,32 @@ class MainWindow(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.setupUi(self)
         
         self.input_folder_button.clicked.connect(self.get_input_folder)
+        self.input_folder_button_2.clicked.connect(self.get_input_folder)
+        self.input_folder_button_3.clicked.connect(self.get_input_folder)
+        self.input_folder_button_4.clicked.connect(self.get_input_folder)
         self.output_folder_button.clicked.connect(self.get_output_folder)
+        self.output_folder_button_2.clicked.connect(self.get_output_folder)
+        self.output_folder_button_3.clicked.connect(self.get_output_folder)
+        self.output_folder_button_4.clicked.connect(self.get_output_folder)
+        
         self.start_button.clicked.connect(self.crop_images)
+        
     
     def get_input_folder(self):
         self.input_directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
         if self.input_directory:
             self.input_path_edit.setText(self.input_directory) 
+            self.input_path_edit_2.setText(self.input_directory)
+            self.input_path_edit_3.setText(self.input_directory)
+            self.input_path_edit_4.setText(self.input_directory)
     
     def get_output_folder(self):
         self.output_directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
         if self.output_directory:
             self.output_path_edit.setText(self.output_directory) 
+            self.output_path_edit_2.setText(self.output_directory) 
+            self.output_path_edit_3.setText(self.output_directory) 
+            self.output_path_edit_4.setText(self.output_directory) 
             
     def crop_images(self):        
         root_path = self.input_path_edit.text()
@@ -31,11 +45,11 @@ class MainWindow(QtWidgets.QMainWindow, design.Ui_MainWindow):
         
         files = next(os.walk(root_path))[2]
         print('Total number of files =',len(files))
-        
+        number_of_file = 0
         for image_file in os.listdir(root_path):            
             image_path = root_path + '/' + image_file
             image = cv2.imread(image_path)
-            
+            number_of_file += 1
             counter = 0
             for r in range(0, image.shape[0], img_height):
                 for c in range(0, image.shape[1], img_width):
@@ -44,12 +58,36 @@ class MainWindow(QtWidgets.QMainWindow, design.Ui_MainWindow):
                     new_image_path = self.output_path_edit.text()+ '/' + str(counter) + '_' +image_file
                     new_image = np.array(image[r:r+img_height, c:c+img_width,:])
                     blank_image[:new_image.shape[0], :new_image.shape[1], :] += new_image
-                    cv2.imwrite(new_image_path, blank_image)
-                    #self.progressBar.setValue(counter)
+                    cv2.imwrite(new_image_path, blank_image)                    
+                    
+            value = number_of_file/len(files)*100
+            self.progressBar.setValue(value)      
             
+    def crop_masks(self):          
+        root_path = self.input_path_edit_2.text()
+        mask_width = int(self.required_width_edit_2.text())
+        mask_height = int(self.required_height_edit_2.text())
+        
+        files = next(os.walk(root_path))[2]
+        print('Total number of files =',len(files))
+        
+        for mask_file in os.listdir(root_path):            
+            mask_path = root_path + '/' + mask_file
+            mask = cv2.imread(mask_path)
+            mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+            mask = mask//38
             
-            print(new_image_path)
-        self.progressBar.setValue(100)
+            counter = 0
+            for r in range(0, mask.shape[0], mask_height):
+                for c in range(0, mask.shape[1], mask_width):
+                    counter += 1
+                    blank_mask = np.zeros((mask_height ,mask_width), dtype = int)
+                    new_mask_path = self.output_path_edit.text()+ '/' + str(counter) + '_' +mask_file
+                    new_mask = np.array(mask[r:r+mask_height, c:c+mask_width])
+                    blank_mask[:new_mask.shape[0], :new_mask.shape[1]] += new_mask
+                    cv2.imwrite(new_mask_path, blank_mask)  
+            value = number_of_file/len(files)*100
+            self.progressBar_2.setValue(value)
 def main():
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
